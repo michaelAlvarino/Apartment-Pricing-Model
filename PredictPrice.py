026e2bf565512fe247e0ecb861789fe9c8cc9cee
mike alvarino <michael.alvarino@gmail.com>
2017-05-19 22:01:03 -0400

    DINING feature is weird, UNITSIZE not as important as expected

--- PredictPrice.py
+++ PredictPrice.py
@@ -2,10 +2,10 @@ import pickle
 from sklearn import preprocessing
 import numpy as np
 
-def predict(MODEL=None, BEDROOMS=1, BATHROOMS=1, UNITSIZE=3, PORCH=1,
-            FIREPLACE=1, DINING=0, FINROOMS=1, HEATTYPE=1,
+def predict(MODEL=None, BEDROOMS=2, BATHROOMS=3, UNITSIZE=4, PORCH=2,
+            FIREPLACE=4, DINING=0, FINROOMS=1, HEATTYPE=1,
             ACPRIMARY=1, FRIDGE=1, KITCHSINK=1, COOKTYPE=1,
-            DISHWASH=0, WASHER=0, DRYER=0, BLD=8, STORIES=4,
+            DISHWASH=1, WASHER=1, DRYER=1, BLD=8, STORIES=4,
             UTILAMT=64, ELECAMT=60, GASAMT=4, WATERAMT=4,
             HOTWATER=1):
     model = MODEL
@@ -20,5 +20,6 @@ def predict(MODEL=None, BEDROOMS=1, BATHROOMS=1, UNITSIZE=3, PORCH=1,
                                     PORCH, FIREPLACE, HEATTYPE, ACPRIMARY,
                                     FRIDGE, HOTWATER, WASHER, DRYER,
                                     DISHWASH, BLD]))
-    return model["model"].predict(X)
+
+    return model["model"].predict(X)[0]
 
